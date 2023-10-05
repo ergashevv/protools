8b1687ecbcbf704bb71569c32a20e2d7e90a6da0
ergashevv <ergashevpulot@gmail.com>
2023-10-05 19:31:07 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, pariatur eum adipisci nesciunt odio modi debitis, incidunt dolor, maxime tempore voluptates quis voluptas. Odit ipsam amet ut et quae laborum.
     </div>
   );
 }
